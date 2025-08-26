421094868b586e53173068ebc131eacda316cbd2
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-08-26 13:22:53 -0700

    feat: Add basic Header component and edit layout.tsx

---
+++ src/app/components/layout/Header/index.tsx
@@ -0,0 +1,30 @@
+import { Antonio } from 'next/font/google';
+import Link from 'next/link';
+import clsx from 'clsx';
+
+const antonio = Antonio({
+  subsets: ['latin'],
+});
+
+export default function Header() {
+  return (
+    <header className='px-4 pt-4 pb-5 sm:px-8 sm:pt-9 sm:pb-7'>
+      <div className='mx-auto flex w-full max-w-94 items-center justify-center sm:max-w-5xl sm:items-baseline'>
+        <Link href={'/'}>
+          <h1
+            className={`${antonio.className} text-3xl sm:text-4xl`}
+            aria-label='Styling Practice by Taka'
+          >
+            <span className='mr-4 tracking-[.18em]'>
+              <span className='text-accent'>S</span>tyling <span className='text-accent'>P</span>
+              ractice
+            </span>
+            <span className={clsx(antonio.className, 'text-lg sm:text-xl tracking-[.1em]')}>
+              by <span className='text-accent'>T</span>AKA
+            </span>
+          </h1>
+        </Link>
+      </div>
+    </header>
+  );
+}
