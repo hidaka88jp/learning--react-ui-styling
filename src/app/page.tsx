5d413f1bb52c8456b92793b84012592ee1f82ebc
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-08-26 13:57:26 -0700

    feat: Add basic Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import Link from 'next/link';
+
+import { SiTailwindcss } from 'react-icons/si';
+import { SiCssmodules } from 'react-icons/si';
+
 export default function Home() {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <section className='px-4 sm:px-8'>
+      <div className='mx-auto w-full max-w-94 sm:max-w-5xl'>
+        <div className='py-16 sm:py-32'>
+          <div className='mb-10 flex flex-col items-center justify-center gap-5 sm:gap-8 sm:flex-row'>
+            <Link
+              href='/tailwind'
+              className='flex items-center gap-2 text-3xl font-medium hover:text-[#06B6D4]'
+            >
+              <SiTailwindcss size={30} />
+              Tailwind CSS
+            </Link>
+            <div className='bg-gray h-0.5 w-6 sm:h-6 sm:w-0.5' />
+            <Link
+              href='/css-modules'
+              className='hover:text-accent flex items-center gap-2 text-3xl font-medium'
+            >
+              <SiCssmodules size={30} />
+              CSS Modules
+            </Link>
+          </div>
+          <h2 className='mb-8 text-center'>Choose a Styling Approach</h2>
+        </div>
+      </div>
+    </section>
   );
 }
